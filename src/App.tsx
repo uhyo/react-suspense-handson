88b21d509b25fff534fe284084986e4332eb7e9b
uhyo <uhyo@uhy.ooo>
2021-12-25 15:52:15 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl">React App!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
