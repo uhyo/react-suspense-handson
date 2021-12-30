4e16025b8f82e584e1e226d38849223093f63708
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-30 22:40:15 +0900

    suspence basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Suspense } from 'react';
 import './App.css'
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const AlwaysSuspend: React.VFC = () => {
+  throw sleep(1000);
+}
 
 function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <AlwaysSuspend />
+      </Suspense>
     </div>
   )
 }
