3051bf86bb153a99c16b6ad341cd16217765ebb1
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-30 22:46:35 +0900

    make error component

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,20 @@ const AlwaysSuspend: React.VFC = () => {
   throw sleep(1000);
 }
 
+const SuspenseError: React.VFC = () => {
+  return (
+    <div>
+      <p>読み込み中...</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
-      <Suspense fallback={<p>Loading...</p>}>
+      {/* <Suspense fallback={<p>Loading...</p>}> */}
+      <Suspense fallback={<SuspenseError />}>
         {/* ↓表示されない */}
         <p>ここは表示される？</p>
         <AlwaysSuspend />
